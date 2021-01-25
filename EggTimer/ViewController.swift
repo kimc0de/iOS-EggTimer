40027decfccfc4427ebf28640c082f537b3b8f32
Kim Ngan <kimngan.ledang@gmail.com>
2021-01-25 22:38:33 +0100

    add timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var secondsRemaining = 60
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate() //stop if button is clicked but previous timer is still running
+        let hardness = sender.currentTitle! // Soft, Medium, Hard
+        secondsRemaining = eggTimes[hardness]!
+        //create a brand new timer
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    @objc func updateTimer(){
+        if secondsRemaining > 0{
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        }else{ //time over
+            timer.invalidate() //stop the timer
+        }
+    }
+    
 }
